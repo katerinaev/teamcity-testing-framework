1b72f9a20832ec864b2491a0cc604da09af59586
katerinaev <katerina.ev@gmail.com>
2024-10-07 19:43:49 +0300

    Add CRUD interface, parametrize request

--- src/main/java/com/example/teamcity/api/models/Steps.java
+++ src/main/java/com/example/teamcity/api/models/Steps.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends BaseModel {
-    private String username;
-    private String password;
+public class Steps {
+    private Integer count;
+    private List<Step> step;
 }
