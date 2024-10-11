205b90a82dad47f9ed48460b40795c3af64ab4a0
katerinaev <katerina.ev@gmail.com>
2024-10-11 11:52:27 +0300

    Add test with facades

--- src/test/java/com/example/teamcity/api/BaseTest.java
+++ src/test/java/com/example/teamcity/api/BaseTest.java
@@ -1,11 +1,14 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.requests.CheckedRequests;
+import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
     protected SoftAssert softy;
+    protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserSpec());
 
     @BeforeMethod(alwaysRun = true)
     public void beforeTest() {
@@ -13,7 +16,8 @@ public class BaseTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void afterTest() {
+    public void afterTest()
+    {
         softy.assertAll();
     }
 }
